a341a79a20483d9f37694181a5439e0066318a58
ASHRAF-BEJJIH <bejjihachraf@gmail.com>
2026-08-27 18:31:23 +0100

    feat : Configiration et connexion avec database

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql2/promise'
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'mol lballon',
+    database: 'redr_db'
+})
+
+
+
+
+
+export default pool
