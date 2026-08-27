7bc3473e61c596cbc51d13c72b0f7a91d1ffeefe
mohamed <mohamedtalbitop@gmail.com>
2026-08-28 00:55:32 +0100

    feat : initialise carService and userService

--- src/service/carService.service.js
+++ src/service/carService.service.js
@@ -1,5 +1,16 @@
-class CarService {
+export default class CarService {
     constructor(carRepository) {
         this.carRepository = carRepository;
+    }
+    getCars = async () => {
+
+    }
+
+    getCarById = async (id) => {
+
+    }
+
+    createCar = async (carData) => {
+        
     }
 }
