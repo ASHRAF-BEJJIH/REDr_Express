90c7ea0c2a531ffb8a60be3e3ac0f384b1398e6d
mohamed <mohamedtalbitop@gmail.com>
2026-08-29 23:27:21 +0100

    refactor(core): reorganize dependency injection container

--- src/core/container.js
+++ src/core/container.js
@@ -1,15 +1,16 @@
-import db from '../config/db.js'
+import { database } from '../config/db.js'
 import UserRepository from '../repository/userRepository.js'
 import CarRepository from '../repository/carRepository.js'
+import ReservationRepository from '../repository/reservationRepository.js'
 import UserService from '../service/userService.service.js'
 import CarService from '../service/carService.service.js'
 import AuthService from '../service/authService.service.js'
 import AuthController from '../controller/authController.controller.js'
 
 // Repositories
-const userRepository = new UserRepository(db)
-const carRepository = new CarRepository(db)
-
+const userRepository = new UserRepository(database.getPool())
+const carRepository = new CarRepository(database.getPool())
+const reservationRepository = new ReservationRepository(database.getPool());
 
 
 // Services
@@ -24,4 +25,4 @@ const authController = new AuthController(authService)
 
 
 
-export {  authController }
+export { authController}
