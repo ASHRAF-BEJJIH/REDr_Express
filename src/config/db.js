5f34a6cdcc638bfdba444016bc725b38cacfd18c
mohamed <mohamedtalbitop@gmail.com>
2026-08-29 23:28:18 +0100

    refactor(database): expose connection pool for dependency injection

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,40 @@
 import mysql from 'mysql2/promise'
 
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Mohamed@99',
-    database: 'redr_db'
-})
+class Database {
+    constructor() {
+        this.pool = mysql.createPool({
+            host: '127.0.0.1',
+            user: 'root',
+            password: 'Mohamed@99',
+            database: 'redr_db'
+        })
+        console.log("-- databse obj created");
+    }
+    getPool() {
+        return this.pool;
+    }
+    async testConnection() {
+        let connection;
+        try {
+            connection = this.getPool().getConnection();
+            console.log("✅ MySQL connected successfully");
+
+            return true;
+
+        } catch (error) {
+            console.error(
+                "❌ MySQL connection failed:",
+                error.message
+            );
+
+        } finally {
+            if (connection) (await connection).release()
+        }
+    }
+}
 
 
 
 
 
-export default pool
+export const database = new Database(); 
